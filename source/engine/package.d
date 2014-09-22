473d1824530544031b926b485b5e1a59104b200a
Justinas Šneideris <develop32@gmail.com>
2014-09-22 16:27:47 +0100

    Added state.d to engine package

--- source/engine/package.d
+++ source/engine/package.d
@@ -5,6 +5,7 @@ public
     import engine.math;
     import engine.render;
     import engine.root;
+    import engine.state;
     import engine.surface;
     import engine.texture;
     import engine.world;
